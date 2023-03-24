32b0b83f255173408190a4252f8e40f5f26514c7
Finley Ge <m13203533462@163.com>
2023-03-24 21:02:06 +0800

    2023-03-24T21:02:06+08:00

--- wezterm.lua
+++ wezterm.lua
@@ -1,26 +1,44 @@
 local wezterm = require 'wezterm'
 
-return {
-  -- color
-  -- color_scheme = "Monokai (terminal.sexy)",
-  -- color_scheme = "MonokaiPro (Gogh)",
-  -- color_scheme = "MonokaiProRistretto (Gogh)",
-  -- color_scheme = "nord",
-  window_background_opacity = 0.8,
-  window_background_image_hsb = {
-    brightness = 0.8,
-    hue = 1.0,
-    saturation = 1.0
-  },
-  -- font
-  font = wezterm.font_with_fallback {
-    'Fira Code Retina',
-    'Symbols Nerd Font',
-    'Noto Sans CJK SC DemiLight',
-  },
-  font_size = 16,
-  harfbuzz_features = { 'liga = 1' },
-  assume_emoji_presentation = true,
-  -- xim_im_name = 'fcitx',
-  -- use_ime = true,
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.window_background_opacity = 0.8
+config.window_background_image_hsb = {
+  brightness = 0.8,
+  hue = 1.0,
+  saturation = 1.0
 }
+
+config.font = wezterm.font_with_fallback {
+  'Fira Code Retina',
+  'Symbols Nerd Font',
+  'Noto Sans CJK SC DemiLight',
+}
+config.font_size = 16
+config.harfbuzz_features = { 'liga = 1' }
+
+return config
+
+-- return {
+--   window_background_opacity = 0.8,
+--   window_background_image_hsb = {
+--     brightness = 0.8,
+--     hue = 1.0,
+--     saturation = 1.0
+--   },
+--   -- font
+--   font = wezterm.font_with_fallback {
+--     'Fira Code Retina',
+--     'Symbols Nerd Font',
+--     'Noto Sans CJK SC DemiLight',
+--   },
+--   font_size = 16,
+--   harfbuzz_features = { 'liga = 1' },
+--   -- assume_emoji_presentation = true,
+--   -- xim_im_name = 'fcitx',
+--   -- use_ime = true,
+-- }
