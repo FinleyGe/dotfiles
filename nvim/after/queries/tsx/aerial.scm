60a23be6e1519283ee8220ae4dd355086246b887
Finley Ge <m13203533462@163.com>
2024-07-24 17:34:39 +0800

    2024-07-24T17:34:39+08:00

---
+++ nvim/after/queries/tsx/aerial.scm
@@ -0,0 +1,11 @@
+;; extends
+(lexical_declaration
+  (
+   variable_declarator
+   value: (
+     call_expression
+     function: (identifier) @name
+     )
+   (#set! "kind" "Constant")
+  )
+)@symbol
